7a6b08a2908b1a62695e26777d68359fdff68c8f
Sycamore1889 <97544810+Sycamore1889@users.noreply.github.com>
2023-07-18 14:17:04 -0700

    Add project files.

---
+++ aspAuthCerner/Pages/Launch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspAuthCerner.Pages
+{
+    public class LaunchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
